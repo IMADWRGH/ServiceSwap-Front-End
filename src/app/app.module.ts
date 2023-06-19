6c3b906184a990b4ed7bd016f4df0d61be1ddc8d
Imad Asserne <imad.asserne@gmail.com>
2023-06-19 04:38:53 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderPublicComponent } from './shared/header/header-public/header-public.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderPublicComponent,
+    HomeComponent,
+    FooterComponent,
+    MenuComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
