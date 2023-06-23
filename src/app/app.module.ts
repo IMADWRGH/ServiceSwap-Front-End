b2470ac8513c5dc25ecc451d711ea0ba4c9906a7
Imad Asserne <imad.asserne@gmail.com>
2023-06-23 06:07:02 +0100

    change navbar & add page "about us"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderPublicComponent } from './shared/header/header-public/header-public.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SearchComponent } from './component/search/search.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderPublicComponent,
     HomeComponent,
     FooterComponent,
     MenuComponent,
     ContactComponent,
     AboutComponent,
-    SearchComponent
+    SearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
