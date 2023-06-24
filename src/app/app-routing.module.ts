b479f4d4f036197497a37d9fb9bbacceada2a04c
Imad Asserne <imad.asserne@gmail.com>
2023-06-24 01:34:47 +0100

    remove menu page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
-import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'menu', component: MenuComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { MenuComponent } from './pages/menu/menu.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { AboutComponent } from './pages/about/about.component';
 import { SearchComponent } from './component/search/search.component';
@@ -16,7 +15,6 @@ import { NavbarComponent } from './shared/navbar/navbar.component';
     AppComponent,
     HomeComponent,
     FooterComponent,
-    MenuComponent,
     ContactComponent,
     AboutComponent,
     SearchComponent,
