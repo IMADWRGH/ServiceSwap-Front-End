797bcedca4b280402e200a50a7194de214d34688
Imad Asserne <imad.asserne@gmail.com>
2023-07-21 18:43:01 +0100

    add npm i --save @auth0/angular-jwt & AuthService

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { User } from '../model/user.module';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthServiceService {
 
-  constructor() { }
+  constructor(public jwtHelper: JwtHelperService) { }
+  public isAuthenticated(): boolean {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
+  }
+
+  public login(username: string, password: string): void {
+    localStorage.setItem('token', 'JWT_TOKEN_FROM_SERVER');
+    localStorage.setItem('user', JSON.stringify({ username, roles: ['admin'] }));
+  }
+
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  public hasRole(role: string): boolean {
+    const user = this.getUser();
+    return user.roles.includes(role);
+  }
+
+  private getUser(): any {
+    const user = localStorage.getItem('user');
+    if (user) {
+      return JSON.parse(user);
+    }
+    return null;
+  }
+
 }
